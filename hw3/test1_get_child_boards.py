3876b7e023b1e9e92b8198a515a47cf09641cd05
Chip <cphchip@gmail.com>
2023-02-23 18:59:13 -0600

    making progress on tic tac toe child boards

--- hw3/test1_get_child_boards.py
+++ hw3/test1_get_child_boards.py
@@ -1,33 +1,43 @@
 # test get_child_boards - separate test file
 import numpy as np
 
+
 def getChildren(board, char):
-    ''' numpy version '''
-    if not char in ['X', 'O']:
+    """numpy version"""
+    if not char in ["X", "O"]:
         raise ValueError("get_child_boards: expecting char='X' or 'O' ")
 
     newval = -1
-    if char == 'X': newval = 1
+    if char == "X":
+        newval = 1
 
     child_list = []
+    zero_values = np.argwhere(board == 0)
+    temp_list = []
 
-    # add your code here
-
+    for indice in zero_values:
+        temp_list = board.tolist()
+        temp_list[indice[0]][indice[1]] = newval
+        child_list.append(temp_list)
     return child_list
+
 
 def run_tests():
     b = np.array([[1, 0, -1], [1, 0, 0], [-1, 0, 0]])
 
-    #TEST1 length of child list
+    # TEST1 length of child list
     expect = b.size - np.count_nonzero(b)
-    child_list = getChildren(b, 'X')
+    child_list = getChildren(b, "X")
 
     if len(child_list) == expect:
-        print (f"PASS Test 1")
-    else: print (f"FAIL Test 1: \
-        expect: {expect} actual: {len(child_list)}")
+        print(f"PASS Test 1")
+    else:
+        print(
+            f"FAIL Test 1: \
+        expect: {expect} actual: {len(child_list)}"
+        )
 
-    #TEST2 - is expected board in list
+    # TEST2 - is expected board in list
     b2 = np.array([[1, 1, -1], [1, 0, 0], [-1, 0, 0]])
     found = False
     for board in child_list:
@@ -36,18 +46,24 @@ def run_tests():
             break
 
     if found:
-        print ("PASS Test 2")
-    else: print (f"FAIL Test 2: Expected board not in child list")
+        print("PASS Test 2")
+    else:
+        print(f"FAIL Test 2: Expected board not in child list")
 
-    #TEST3 Test 4x4 array
-    b3 = np.array([ [0,0,0,0], [0,0,0,0],[0,0,0,0],[0,0,0,0] ])
+    # TEST3 Test 4x4 array
+    b3 = np.array(
+        [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    )
     expect = b.size - np.count_nonzero(b)
-    child_list = getChildren(b, 'X')
+    child_list = getChildren(b, "X")
 
     if len(child_list) == expect:
         print(f"PASS Test 3  4x4 array")
     else:
-        print(f"FAIL Test 3 4x4 array: \
-            expect: {expect} actual: {len(child_list)}")
+        print(
+            f"FAIL Test 3 4x4 array: \
+            expect: {expect} actual: {len(child_list)}"
+        )
+
 
 run_tests()
--- hw3/test2_evaluate.py
+++ hw3/test2_evaluate.py
@@ -1,156 +1,119 @@
 import numpy as np
 
+
 def evaluate(board):
-    '''returns 1 for X win, -1 for O win, 0 for tie OR game in progress
+    """returns 1 for X win, -1 for O win, 0 for tie OR game in progress
     Using numpy functions to add values in rows and cols
     If we get a sum equal to size of row,col,diag (plus or minus)
      we have a winner
-    '''
-
-    '''
+    """
     i = 0
-    j = 0
+    while i <= board.shape[0] - 1:
 
-    while i <= 2:
-        while j <= 2:
-            if ((board[i][j] == board[i][j+1] and 
-                board[i][j+1] == board[i][j+2]) or 
-                (board[i][j] == board[i+1][j] and 
-                board[i+1][j] == board[i+2][j])):
-
-                if board[i][j] == 1:
-                    return 1
-                elif board[i][j] == -1:
-                    return -1
-                elif board[i][j] == 0:
-                    return 0
-        j += 1
-    i += 1
-    '''
-    
-    i = 0
-    print(np.sum(board,0)[0]) # sum the first column
-    # FYI Axis 0 in numpy refers to columns
-    # Axis 1 refers to rows
-    
-    while i <= 2:
-        print (np.sum(board,0)[i] == 1)
-        print (np.sum(board,1)[i])
-        if np.sum(board,0)[i] == 3 or np.sum(board,1)[i] == 3:
+        if (
+            np.sum(board, 0)[i] == board.shape[0]
+            or np.sum(board, 1)[i] == board.shape[0]
+            or np.sum(np.diagonal(board)) == board.shape[0]
+            or np.sum(np.fliplr(board).diagonal()) == board.shape[0]
+        ):
             return 1
-        elif np.sum(board,0)[i] == -3 or np.sum(board,1)[i] == -3:
+
+        elif (
+            np.sum(board, 0)[i] == -1 * board.shape[0]
+            or np.sum(board, 1)[i] == -1 * board.shape[0]
+            or np.sum(np.diagonal(board)) == -1 * board.shape[0]
+            or np.sum(np.fliplr(board).diagonal())
+            == -1 * board.shape[0]
+        ):
             return -1
-
         i += 1
+
     return 0
-
-    i = 0
-    j = 0
-
-    while i <= 2: # Detect row win
-
-        if (board[i][j] == board[i][j+1] and 
-            board[i][j+1] == board[i][j+2]): 
-
-            if board[i][j] == 1:
-                return 1
-            elif board[i][j] == -1:
-                return -1
-            elif board[i][j] == 0:
-                return 0
-            
-        i += 1
-
-
-    i = 0
-    j = 0
-
-    while j <= 2: # Detect column win
-        
-        if (board[i][j] == board[i+1][j] and 
-            board[i+1][j] == board[i+2][j]):
-
-            if board[i][j] == 1: 
-                return 1
-            elif board[i][j] == -1:
-                return -1
-            elif board[i][j] == 0:
-                return 0
-                    
-        j += 1
-
-    # Detect diagaonal win
-    if board[0][0] == board [1][1] and board[1][1] == board[2][2]:
-        return board[0][0]
-    elif board[0][2] == board[1][2] and board[1][2] == board[2][0]:
-        return board[0][2]
 
 
 #### TEST CODE ##########
 def run_tests():
     b = np.array([[1, 0, -1], [1, 0, 0], [-1, 0, 0]])
 
-    #TEST1 : No winner
+    # TEST1 : No winner
     b = np.array([[1, 0, -1], [1, 0, 0], [-1, 0, 0]])
     score = evaluate(b)
     expect = 0
 
     if score == expect:
-        print (f"PASS Test 1 No Win")
-    else: print (f"FAIL Test 1 No Win: \
-        expect: {expect} actual: {score}")
+        print(f"PASS Test 1 No Win")
+    else:
+        print(
+            f"FAIL Test 1 No Win: \
+        expect: {expect} actual: {score}"
+        )
 
-    #TEST 2: Win for X
+    # TEST 2: Win for X
     b = np.array([[1, 0, -1], [1, 0, 0], [1, 0, -1]])
     score = evaluate(b)
     expect = 1
 
     if score == expect:
-        print (f"PASS Test 2  column win")
-    else: print (f"FAIL Test 2: column win \
-        expect: {expect} actual: {score}")
+        print(f"PASS Test 2  column win")
+    else:
+        print(
+            f"FAIL Test 2: column win \
+        expect: {expect} actual: {score}"
+        )
 
-    #TEST3 Win for O
+    # TEST3 Win for O
     b = np.array([[-1, -1, -1], [1, 0, 1], [1, 0, 1]])
     score = evaluate(b)
     expect = -1
 
     if score == expect:
-        print (f"PASS Test 3  row win")
-    else: print (f"FAIL Test 3: row win \
-        expect: {expect} actual: {score}")
+        print(f"PASS Test 3  row win")
+    else:
+        print(
+            f"FAIL Test 3: row win \
+        expect: {expect} actual: {score}"
+        )
 
-
-    #TEST4 Win for X on diagonal
+    # TEST4 Win for X on diagonal
     b = np.array([[-1, -1, 1], [1, 1, 1], [1, 0, -1]])
     score = evaluate(b)
     expect = 1
 
     if score == expect:
-        print (f"PASS Test 4  diag win")
-    else: print (f"FAIL Test 4: diag win \
-        expect: {expect} actual: {score}")
+        print(f"PASS Test 4  diag win")
+    else:
+        print(
+            f"FAIL Test 4: diag win \
+            expect: {expect} actual: {score}"
+        )
 
-    #TEST5 win for O on reverse diagonal
+    # TEST5 win for O on reverse diagonal
     b = np.array([[-1, 1, 1], [1, -1, -1], [1, 0, -1]])
     score = evaluate(b)
     expect = -1
 
     if score == expect:
-        print (f"PASS Test 5  diag2 win")
-    else: print (f"FAIL Test 5: diag2 win \
-        expect: {expect} actual: {score}")
+        print(f"PASS Test 5  diag2 win")
+    else:
+        print(
+            f"FAIL Test 5: diag2 win \
+        expect: {expect} actual: {score}"
+        )
 
-    #TEST6 win for O on reverse diagonal for 4x4 board
-    b = np.array([[-1, 1, 1, 0], [1, -1, -1, 0], \
-                  [1, 0, -1, 0], [1,0,0,-1]])
+    # TEST6 win for O on reverse diagonal for 4x4 board
+    b = np.array(
+        [[-1, 1, 1, 0], [1, -1, -1, 0], [1, 0, -1, 0], [1, 0, 0, -1]]
+    )
     score = evaluate(b)
     expect = -1
 
     if score == expect:
-        print (f"PASS Test 6  diag2 win 4x4")
-    else: print (f"FAIL Test 6: diag2 win 4x4 \
-        expect: {expect} actual: {score}")
+        print(f"PASS Test 6  diag2 win 4x4")
+    else:
+        print(
+            f"FAIL Test 6: diag2 win 4x4 \
+        expect: {expect} actual: {score}"
+        )
 
 
 run_tests()
