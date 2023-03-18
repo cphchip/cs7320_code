d60caf2e75e24a261a21d28090304026bd0ce11b
Chip <cphchip@gmail.com>
2023-03-18 16:12:53 -0500

    adding final comments

--- MidtrmFiles/RoboVac4.py
+++ MidtrmFiles/RoboVac4.py
@@ -33,7 +33,7 @@ class RoboVac:
             for y in range(self.room_height):
                 self.free_tiles_set.add((x, y))
 
-        # Copied code from Pygame to determine blocked cells
+        # Copied code from Pygame to determine blocked tiles
         for b in self.block_list:
             for x in range(b[0], b[0] + b[2]):
                 for y in range(b[1], b[1] + b[3]):
@@ -58,8 +58,11 @@ class RoboVac:
         global call_count
         global move_list
 
+        # First pass enter search
+        # Subsequent passes return values
         if call_count == 0:
-            move_list = self.next_step_dfs(vac_pos) # Change to alter search
+            # Change to alter search, bfs or dfs
+            move_list = self.next_step_dfs(vac_pos) 
         call_count += 1
         
         return move_list[call_count]
@@ -67,56 +70,68 @@ class RoboVac:
 
     def next_step_bfs(self, vac_pos):
 
-        '''Copied from 8-game - BFS'''
+        '''Copied from 8-game and modified - BFS'''
         global visited_set
+        # Convert position to array format (row, col) from (x, y)
         arr_pos = vac_pos[::-1]
-        queue = [[(None, arr_pos, floor)]]
+        # Queue holds (move, location, floor array) as tuple
+        queue = [[(None, arr_pos, floor)]] 
+        # Free cell qty used to determine if goal board is reached
         goal_board = len(self.free_tiles_set)
         final_path = []
 
         while queue:
+            # Pull first path from queue
             path = queue.pop(0)
-            # Clear visited set so only traversed tiles are saved            
+            # Rebuild visited set for newest path            
             visited_set.clear() 
             for items in path:
-                visited_set.add((items[1])) # In array format (row, col)
-            vertex = path[-1][2]
-            cur_pos = path[-1][1] 
+                visited_set.add((items[1]))
+            vertex = path[-1][2] # Get floor from tuple
+            cur_pos = path[-1][1] # Get position from tuple
+            # Returns [move, location, new floor] for each child
             child_list = self.get_child_floor_list(cur_pos, vertex)
-
             next_node_list = ([x for x in child_list 
                                if x[2] not in path])
-
             for next in next_node_list:
-                if np.sum(next[2]) == goal_board or len(path) == 400:
+                # Visited floor spaces are value 1, compare sum with free tiles qty to check solution
+                # Cutoff set to 17 due to bfs memory/time consumption
+                if np.sum(next[2]) == goal_board or len(path) == 17:
                     for x in path:
                         final_path.append(x[0])
                     return final_path
                 else:
                     queue.append(path + [next])
     
-    '''Copied from 8 game DFS'''
+    
     def next_step_dfs(self, vac_pos):
+        
+        '''Copied from 8 game and modified - DFS'''
         global visited_set
+        # Convert position to array format (row, col) from (x, y)
         arr_pos = vac_pos[::-1]
+        # stack holds (move, location, floor array) as tuple
         stack = [[(None, arr_pos, floor)]]
+        # Free cell qty used to determine if goal board is reached
         goal_board = len(self.free_tiles_set)
         final_path = []
 
         while stack:
+            # Pull last path added to stack
             path = stack.pop()
-            # Clear visited set so only traversed tiles are saved            
+            # Rebuild visited set for newest path            
             visited_set.clear() 
             for items in path:
-                visited_set.add((items[1])) # In array format (row, col)
-            vertex = path[-1][2]
-            cur_pos = path[-1][1] 
+                visited_set.add((items[1]))
+            vertex = path[-1][2] # Get floor from tuple
+            cur_pos = path[-1][1] # Get position from tuple
+            # Returns [move, location, new floor] for each child
             child_list = self.get_child_floor_list(cur_pos, vertex)
-
             next_node_list = ([x for x in child_list 
                                if x[2] not in path])
 
             for next in next_node_list:
+                # Visited floor spaces are value 1, compare sum with free tiles qty to check solution, or cutoff at 400
                 if np.sum(next[2]) == goal_board or len(path) == 400:
                     for x in path:
                         final_path.append(x[0])
@@ -131,6 +146,7 @@ class RoboVac:
         array_blocked_tiles = set()
         moves = []
 
+        # Convert blocked tiles from (x, y) to array format (row,col)
         for b_tiles in self.blocked_tiles_set:
             array_blocked_tiles.add(b_tiles[::-1])
 
@@ -144,20 +160,26 @@ class RoboVac:
         if col > 0:
             move_west = (row, col - 1)
             moves.append((move_west))
+
         if col < max_col:
             move_east = (row, col + 1)
             moves.append((move_east))
+
         if row > 0:
             move_north = (row - 1, col)
             moves.append((move_north))
+
         if row < max_row:
             move_south = (row + 1, col)
             moves.append((move_south))
 
         # Consider board conditions for possible moves, 9 total
-        if (row > 0 and row < max_row
-            and col > 0 and col < max_col
+        if (row > 0 
+            and row < max_row
+            and col > 0 
+            and col < max_col
         ):  # Center condition
+            
             if (move_north not in array_blocked_tiles 
                 and move_north not in visited_set
             ):
@@ -182,7 +204,7 @@ class RoboVac:
                 child3 = self.child_gen(3, current_floor, move_west)
                 children.append(child3)
 
-            if not children: # Get un-stuck
+            if not children: # Get un-stuck with random move
                 options = ([x for x in moves 
                             if x not in array_blocked_tiles])
                 choice = random.choice(options)
@@ -201,6 +223,7 @@ class RoboVac:
                     children.append(child3)
                             
         elif row == 0 and col > 0 and col < max_col:  # Top edge 
+            
             if (move_east not in array_blocked_tiles 
                 and move_east not in visited_set
             ):
@@ -219,7 +242,7 @@ class RoboVac:
                 child3 = self.child_gen(3, current_floor, move_west)
                 children.append(child3)
 
-            if not children: # Get un-stuck
+            if not children: # Get un-stuck with random move
                 options = ([x for x in moves 
                             if x not in array_blocked_tiles])
                 choice = random.choice(options)
@@ -234,7 +257,11 @@ class RoboVac:
                     child3 = self.child_gen(3, current_floor, move_west)
                     children.append(child3)
                     
-        elif row > 0 and col == 0 and row < max_row: # Left edge 
+        elif (row > 0 
+              and col == 0 
+              and row < max_row
+        ): # Left edge 
+            
             if (move_north not in array_blocked_tiles 
                 and move_north not in visited_set
             ):
@@ -253,7 +280,7 @@ class RoboVac:
                 child2 = self.child_gen(2, current_floor, move_south)
                 children.append(child2)
 
-            if not children: # Get un-stuck
+            if not children: # Get un-stuck with random move
                 options = [x for x in moves if x not in array_blocked_tiles]
                 choice = random.choice(options)
 
@@ -267,7 +294,11 @@ class RoboVac:
                     child2 = self.child_gen(2, current_floor, move_south)
                     children.append(child2)
             
-        elif row == max_row and col > 0 and col < max_col: # Bottom edge 
+        elif (row == max_row 
+              and col > 0 
+              and col < max_col
+
+        ): # Bottom Edge
             if (move_north not in array_blocked_tiles 
                 and move_north not in visited_set
             ):
@@ -286,7 +317,7 @@ class RoboVac:
                 child3 = self.child_gen(3, current_floor, move_west)
                 children.append(child3)
 
-            if not children: # Get un-stuck
+            if not children: # Get un-stuck with random move
                 options = [x for x in moves if x not in array_blocked_tiles]
                 choice = random.choice(options)
 
@@ -300,7 +331,11 @@ class RoboVac:
                     child3 = self.child_gen(3, current_floor, move_west)
                     children.append(child3)
             
-        elif row > 0 and row < max_row and col == max_col: # Right edge
+        elif (row > 0 
+              and row < max_row 
+              and col == max_col
+        ): # Right edge
+            
             if (move_north not in array_blocked_tiles 
                 and move_north not in visited_set
             ):
@@ -319,7 +354,7 @@ class RoboVac:
                 child3 = self.child_gen(3, current_floor, move_west)
                 children.append(child3)
 
-            if not children: # Get un-stuck
+            if not children: # Get un-stuck with random move
                 options = [x for x in moves if x not in array_blocked_tiles]
                 choice = random.choice(options)
 
@@ -334,6 +369,7 @@ class RoboVac:
                     children.append(child3)
             
         elif row == max_row and col == max_col: # Bottom right corner 
+
             if (move_north not in array_blocked_tiles 
                 and move_north not in visited_set
             ):
@@ -346,7 +382,7 @@ class RoboVac:
                 child3 = self.child_gen(3, current_floor, move_west)
                 children.append(child3)
 
-            if not children: # Get un-stuck
+            if not children: ## Get un-stuck with random move
                 options = [x for x in moves if x not in array_blocked_tiles]
                 choice = random.choice(options)
 
@@ -358,6 +394,7 @@ class RoboVac:
                     children.append(child3)
             
         elif row == 0 and col == max_col: # Top right corner
+
             if (move_south not in array_blocked_tiles 
                 and move_south not in visited_set
             ):
@@ -382,6 +419,7 @@ class RoboVac:
                     children.append(child3)
             
         elif row == 0 and col == 0: # Top left corner
+
             if (move_east not in array_blocked_tiles 
                 and move_east not in visited_set
             ):
@@ -394,7 +432,7 @@ class RoboVac:
                 child2 = self.child_gen(2, current_floor, move_south)
                 children.append(child2)
 
-            if not children: # Get un-stuck
+            if not children: # Get un-stuck with random move
                 options = [x for x in moves if x not in array_blocked_tiles]
                 choice = random.choice(options)
 
@@ -406,6 +444,7 @@ class RoboVac:
                     children.append(child2)
 
         elif row == max_row and col == 0: # Bottom left corner
+
             if (move_north not in array_blocked_tiles 
                 and move_north not in visited_set
             ):
@@ -418,7 +457,7 @@ class RoboVac:
                 child1 = self.child_gen(1, current_floor, move_east)
                 children.append(child1)
 
-            if not children: # Get un-stuck
+            if not children: # Get un-stuck with random move
                 options = [x for x in moves if x not in array_blocked_tiles]
                 choice = random.choice(options)
 
@@ -433,24 +472,23 @@ class RoboVac:
         
 
     def child_gen(self, move, current_floor, new_pos):
-        global visited_set
 
         if move == 0:
             child0 = copy.deepcopy(current_floor)
             child0[new_pos] = 1
-            return [move, (new_pos), child0.tolist()]
+            return [move, (new_pos), child0]
         
         elif move == 1:
             child1 = copy.deepcopy(current_floor)
             child1[new_pos] = 1
-            return [move, (new_pos), child1.tolist()]
+            return [move, (new_pos), child1]
         
         elif move == 2:
             child2 = copy.deepcopy(current_floor)
             child2[new_pos] = 1
-            return [move, (new_pos), child2.tolist()]
+            return [move, (new_pos), child2]
         
         elif move == 3:
             child3 = copy.deepcopy(current_floor)
             child3[new_pos] = 1
-            return [move, (new_pos), child3.tolist()]
+            return [move, (new_pos), child3]
