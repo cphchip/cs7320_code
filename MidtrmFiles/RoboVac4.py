4a72af20db0b132d36304cd9119aa253ec3f4861
Chip-PC <cphchip@gmail.com>
2023-03-18 08:13:49 -0500

    finally close to a final solution

--- MidtrmFiles/RoboVac4.py
+++ MidtrmFiles/RoboVac4.py
@@ -12,9 +12,9 @@ import random
 import numpy as np
 import copy
 
+# Global variable declarations
 visited_set = set()
 floor = np.array
-# queue = []
 move_list = []
 call_count = 0
 
@@ -28,7 +28,6 @@ class RoboVac:
         self.free_tiles_set = set()
 
         # Copied code from Pygame to determine all tiles
-        # Note this may need to to different function if updates are needed at each turn
         self.free_tiles_set = set()
         for x in range(self.room_width):
             for y in range(self.room_height):
@@ -50,11 +49,6 @@ class RoboVac:
         for b_tile in self.blocked_tiles_set:
             floor[b_tile[1], b_tile[0]] = -1
 
-        # global visited_set
-        # # Treat blocked as visited for simplicity
-        # for tile in self.blocked_tiles_set:
-        #     visited_set.add(tile) 
-
         self.name = "Chip Henderson"
         self.id = "48996654"
 
@@ -74,11 +68,9 @@ class RoboVac:
     def next_step(self, vac_pos):
 
         '''Copied from 8-game - BFS'''
-        # global queue
         global visited_set
-        # curr_pos = vac_pos
         arr_pos = vac_pos[::-1]
-        queue = [[(None, arr_pos, floor)]] # If this causes error put floor.tolist() back
+        queue = [[(None, arr_pos, floor)]]
         goal_board = len(self.free_tiles_set)
         final_path = []
 
@@ -90,11 +82,10 @@ class RoboVac:
             cur_pos = path[-1][1] 
             child_list = self.get_child_floor_list(cur_pos, vertex)
 
-            # debug_var = len(path)
             next_node_list = [x for x in child_list if x[2] not in path]
 
             for next in next_node_list:
-                if np.sum(next[2]) == goal_board or len(path) == 20:
+                if np.sum(next[2]) == goal_board or len(path) == 400:
                     for x in path:
                         final_path.append(x[0])
                     return final_path
@@ -103,19 +94,17 @@ class RoboVac:
     
     def get_child_floor_list(self, arr_pos, current_floor):
 
-        # print(current_floor)
         max_row, max_col = self.room_height - 1, self.room_width - 1
         current_floor = np.array(current_floor)
         array_blocked_tiles = set()
-        array_visited_tiles = set()
+        # array_visited_tiles = set()
+        moves = []
+
         for b_tiles in self.blocked_tiles_set:
             array_blocked_tiles.add(b_tiles[::-1])
 
-        # for v_tiles in visited_set:
-        #     array_visited_tiles.add((v_tiles))
-        array_visited_tiles = visited_set
+        # visited_set = visited_set
 
-        # array_blocked_tiles = reversed(self.blocked_tiles_set)
         children = []
 
         """Uses code modified from 8-game homework"""
@@ -125,224 +114,317 @@ class RoboVac:
         # Create new positions
         if col > 0:
             move_west = (row, col - 1)
+            moves.append((move_west))
         if col < max_col:
             move_east = (row, col + 1)
+            moves.append((move_east))
         if row > 0:
             move_north = (row - 1, col)
+            moves.append((move_north))
         if row < max_row:
             move_south = (row + 1, col)
+            moves.append((move_south))
 
         # Consider board conditions for possible moves
         if (row > 0 and row < max_row
             and col > 0 and col < max_col
         ):  # Center condition
             if (move_north not in array_blocked_tiles 
-                and move_north not in array_visited_tiles
+                and move_north not in visited_set
             ):
                 child0 = self.child_gen(0, current_floor, move_north)
                 children.append(child0)
 
             if (move_east not in array_blocked_tiles 
-                and move_east not in array_visited_tiles
+                and move_east not in visited_set
             ):
                 child1 = self.child_gen(1, current_floor, move_east)
                 children.append(child1)
 
             if (move_south not in array_blocked_tiles 
-                and move_south not in array_visited_tiles
+                and move_south not in visited_set
             ):
                 child2 = self.child_gen(2, current_floor, move_south)
                 children.append(child2)
 
             if (move_west not in array_blocked_tiles 
-                and move_west not in array_visited_tiles
+                and move_west not in visited_set
             ):
                 child3 = self.child_gen(3, current_floor, move_west)
                 children.append(child3)
-            
-            # return children
+
+            if not children: # Get un-stuck
+                options = [x for x in moves if x not in array_blocked_tiles]
+                choice = random.choice(options)
+
+                if choice == move_north:
+                    child0 = self.child_gen(0, current_floor, move_north)
+                    children.append(child0)
+                elif choice == move_east:
+                    child1 = self.child_gen(1, current_floor, move_east)
+                    children.append(child1)
+                elif choice == move_south:
+                    child2 = self.child_gen(2, current_floor, move_south)
+                    children.append(child2)
+                elif choice == move_west:
+                    child3 = self.child_gen(3, current_floor, move_west)
+                    children.append(child3)
         
         elif row == 0 and col == 0:  # Upper left corner 
             if (move_east not in array_blocked_tiles 
-                and move_east not in array_visited_tiles
+                and move_east not in visited_set
             ):
                 child1 = self.child_gen(1, current_floor, move_east)
                 children.append(child1)
             
             if (move_south not in array_blocked_tiles 
-                and move_south not in array_visited_tiles
+                and move_south not in visited_set
             ):
                 child2 = self.child_gen(2, current_floor, move_south)
                 children.append(child2)
-            
-            # return children
-        
+
+            if not children: # Get un-stuck
+                options = [x for x in moves if x not in array_blocked_tiles]
+                choice = random.choice(options)
+
+                if choice == move_east:
+                    child1 = self.child_gen(1, current_floor, move_east)
+                    children.append(child1)
+                elif choice == move_south:
+                    child2 = self.child_gen(2, current_floor, move_south)
+                    children.append(child2)
+                    
         elif row == 0 and col > 0 and col < max_col:  # Top edge 
             if (move_east not in array_blocked_tiles 
-                and move_east not in array_visited_tiles
+                and move_east not in visited_set
             ):
                 child1 = self.child_gen(1, current_floor, move_east)
                 children.append(child1)
             
             if (move_south not in array_blocked_tiles 
-                and move_south not in array_visited_tiles
+                and move_south not in visited_set
             ):
                 child2 = self.child_gen(2, current_floor, move_south)
                 children.append(child2)
             
-            if move_west not in array_blocked_tiles and move_west not in array_visited_tiles:
+            if (move_west not in array_blocked_tiles 
+                and move_west not in visited_set
+            ):
                 child3 = self.child_gen(3, current_floor, move_west)
                 children.append(child3)
-            
-            # return children
-        
+
+            if not children: # Get un-stuck
+                options = [x for x in moves if x not in array_blocked_tiles]
+                choice = random.choice(options)
+
+                if choice == move_east:
+                    child1 = self.child_gen(1, current_floor, move_east)
+                    children.append(child1)
+                elif choice == move_south:
+                    child2 = self.child_gen(2, current_floor, move_south)
+                    children.append(child2)
+                elif choice == move_west:
+                    child3 = self.child_gen(3, current_floor, move_west)
+                    children.append(child3)
+                    
         elif row > 0 and col == 0 and row < max_row: # Left edge 
-            if move_north not in array_blocked_tiles and move_north not in array_visited_tiles:
+            if (move_north not in array_blocked_tiles 
+                and move_north not in visited_set
+            ):
                 child0 = self.child_gen(0, current_floor, move_north)
                 children.append(child0)
 
             if (move_east not in array_blocked_tiles 
-                  and move_east not in array_visited_tiles
+                  and move_east not in visited_set
             ):
                 child1 = self.child_gen(1, current_floor, move_east)
                 children.append(child1)
 
             if (move_south not in array_blocked_tiles 
-                  and move_south not in array_visited_tiles
+                  and move_south not in visited_set
             ):
                 child2 = self.child_gen(2, current_floor, move_south)
                 children.append(child2)
-            
-            # return children
 
+            if not children: # Get un-stuck
+                options = [x for x in moves if x not in array_blocked_tiles]
+                choice = random.choice(options)
+
+                if choice == move_north:
+                    child0 = self.child_gen(0, current_floor, move_north)
+                    children.append(child0)
+                elif choice == move_east:
+                    child1 = self.child_gen(1, current_floor, move_east)
+                    children.append(child1)
+                elif choice == move_south:
+                    child2 = self.child_gen(2, current_floor, move_south)
+                    children.append(child2)
+            
         elif row == max_row and col > 0 and col < max_col: # Bottom edge 
             if (move_north not in array_blocked_tiles 
-                and move_north not in array_visited_tiles
+                and move_north not in visited_set
             ):
                 child0 = self.child_gen(0, current_floor, move_north)
                 children.append(child0)
 
-            if (move_east not in array_blocked_tiles and move_east not in array_visited_tiles
+            if (move_east not in array_blocked_tiles 
+                and move_east not in visited_set
             ):
                 child1 = self.child_gen(1, current_floor, move_east)
                 children.append(child1)
 
             if (move_west not in array_blocked_tiles 
-                  and move_west not in array_visited_tiles
+                  and move_west not in visited_set
             ):
                 child3 = self.child_gen(3, current_floor, move_west)
                 children.append(child3)
-            
-            # return children
 
+            if not children: # Get un-stuck
+                options = [x for x in moves if x not in array_blocked_tiles]
+                choice = random.choice(options)
+
+                if choice == move_north:
+                    child0 = self.child_gen(0, current_floor, move_north)
+                    children.append(child0)
+                elif choice == move_east:
+                    child1 = self.child_gen(1, current_floor, move_east)
+                    children.append(child1)
+                elif choice == move_west:
+                    child3 = self.child_gen(3, current_floor, move_west)
+                    children.append(child3)
+            
         elif row > 0 and row < max_row and col == max_col: # Right edge
             if (move_north not in array_blocked_tiles 
-                and move_north not in array_visited_tiles
+                and move_north not in visited_set
             ):
                 child0 = self.child_gen(0, current_floor, move_north)
                 children.append(child0)
 
             if (move_south not in array_blocked_tiles 
-                  and move_south not in array_visited_tiles
+                  and move_south not in visited_set
             ):
                 child2 = self.child_gen(2, current_floor, move_south)
                 children.append(child2)
 
             if (move_west not in array_blocked_tiles 
-                  and move_west not in array_visited_tiles
+                  and move_west not in visited_set
             ):
                 child3 = self.child_gen(3, current_floor, move_west)
                 children.append(child3)
-            
-            # return children
 
+            if not children: # Get un-stuck
+                options = [x for x in moves if x not in array_blocked_tiles]
+                choice = random.choice(options)
+
+                if choice == move_north:
+                    child0 = self.child_gen(0, current_floor, move_north)
+                    children.append(child0)
+                elif choice == move_south:
+                    child2 = self.child_gen(2, current_floor, move_south)
+                    children.append(child2)
+                elif choice == move_west:
+                    child3 = self.child_gen(3, current_floor, move_west)
+                    children.append(child3)
+            
         elif row == max_row and col == max_col: # Bottom right corner 
             if (move_north not in array_blocked_tiles 
-                and move_north not in array_visited_tiles
+                and move_north not in visited_set
             ):
                 child0 = self.child_gen(0, current_floor, move_north)
                 children.append(child0)
 
             if (move_west not in array_blocked_tiles 
-                  and move_west not in array_visited_tiles
+                  and move_west not in visited_set
             ):
                 child3 = self.child_gen(3, current_floor, move_west)
                 children.append(child3)
-            
-            # return children
 
+            if not children: # Get un-stuck
+                options = [x for x in moves if x not in array_blocked_tiles]
+                choice = random.choice(options)
+
+                if choice == move_north:
+                    child0 = self.child_gen(0, current_floor, move_north)
+                    children.append(child0)
+                elif choice == move_west:
+                    child3 = self.child_gen(3, current_floor, move_west)
+                    children.append(child3)
+            
         elif row == 0 and col == max_col: # Top right corner
             if (move_south not in array_blocked_tiles 
-                and move_south not in array_visited_tiles
+                and move_south not in visited_set
             ):
                 child2 = self.child_gen(2, current_floor, move_south)
                 children.append(child2)
 
             if (move_west not in array_blocked_tiles 
-                and move_west not in array_visited_tiles
+                and move_west not in visited_set
             ):
                 child3 = self.child_gen(3, current_floor, move_west)
                 children.append(child3)
-            
-            # return children
 
+            if not children: # Get un-stuck
+                options = [x for x in moves if x not in array_blocked_tiles]
+                choice = random.choice(options)
+
+                if choice == move_south:
+                    child2 = self.child_gen(2, current_floor, move_south)
+                    children.append(child2)
+                elif choice == move_west:
+                    child3 = self.child_gen(3, current_floor, move_west)
+                    children.append(child3)
+            
         elif row == 0 and col == 0: # Top left corner
             if (move_east not in array_blocked_tiles 
-                and move_east not in array_visited_tiles
+                and move_east not in visited_set
             ):
                 child1 = self.child_gen(1, current_floor, move_east)
                 children.append(child1)
 
-            if (move_south not in array_blocked_tiles and move_south not in array_visited_tiles
+            if (move_south not in array_blocked_tiles 
+                and move_south not in visited_set
             ):
                 child2 = self.child_gen(2, current_floor, move_south)
                 children.append(child2)
-            
-            # return children
+
+            if not children: # Get un-stuck
+                options = [x for x in moves if x not in array_blocked_tiles]
+                choice = random.choice(options)
+
+                if choice == move_east:
+                    child1 = self.child_gen(1, current_floor, move_east)
+                    children.append(child1)
+                elif choice == move_south:
+                    child2 = self.child_gen(2, current_floor, move_south)
+                    children.append(child2)
 
         elif row == max_row and col == 0: # Bottom left corner
             if (move_north not in array_blocked_tiles 
-                and move_north not in array_visited_tiles
+                and move_north not in visited_set
             ):
                 child0 = self.child_gen(0, current_floor, move_north)
                 children.append(child0)
 
             if (move_east not in array_blocked_tiles 
-                and move_east not in array_visited_tiles
+                and move_east not in visited_set
             ):
                 child1 = self.child_gen(1, current_floor, move_east)
                 children.append(child1)
-            
-        if not children: # get un-stuck
-            choice = random.choice([0, 1, 2, 3])
 
-            try:    
-                if choice == 0 and move_north not in array_blocked_tiles:
+            if not children: # Get un-stuck
+                options = [x for x in moves if x not in array_blocked_tiles]
+                choice = random.choice(options)
+
+                if choice == move_north:
                     child0 = self.child_gen(0, current_floor, move_north)
                     children.append(child0)
-            except:
-                next
-            try:
-                if choice == 1 and move_east not in array_blocked_tiles:
+                elif choice == move_east:
                     child1 = self.child_gen(1, current_floor, move_east)
                     children.append(child1)
-            except:
-                next
-            try:
-                if choice == 2 and move_south not in array_blocked_tiles:
-                    child2 = self.child_gen(2, current_floor, move_south)
-                    children.append(child2)
-            except:
-                next
-            try:
-                if choice == 3 and move_west not in array_blocked_tiles:
-                    child3 = self.child_gen(3, current_floor, move_west)
-                    children.append(child3)
-            except:
-                exit
 
-        # if not children: # Debug
-        #     print("check children") # Debug
+        if not children: # Debug
+            print("check children") # Debug
+
         return children
         
 
